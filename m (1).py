0b290d9f848867ed9c5e243013e99c5274085483
bikeym197 <bikeym197@gmail.com>
2025-01-15 18:34:27 +0530

    Update m.py

--- m (1).py
+++ m (1).py
@@ -8,10 +8,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7267073687:AAFVLtkFEldL6R4jwT_1Z4T2drZSx4roIm4')
+bot = telebot.TeleBot('7504464475:AAENIf9JoOvBck_qT4bbyZ0Mq016q0q9QRA')
 
 # Admin user IDs
-admin_id = ['6755603559']
+admin_id = ['1263413072']
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
